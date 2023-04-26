b79aec9092dda789c691b559904d92916ec5f282
Ayushi Kunwar <85884534+Ayushik703@users.noreply.github.com>
2023-04-26 22:14:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// let packages = document.querySelector('#packages-here');
+
+// packages.addEventListener('click', function () {
+//     document.getElementById("package-id").classList.toggle("show");
+
+
+//     // if (document.querySelector('.packages').style.display === "none") {
+//     //     document.querySelector('.packages').style.display = "block";
+//     // } else {
+//     //     document.querySelector('.packages').style.display = "none";
+//     // }
+// })
+
+
+// window.onclick = function (event) {
+//     if (!event.target.matches('#packages-here')) {
+//         var dropdowns = document.querySelector('.packages');
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
